ba5d248b539fa7b38ed3febf8e06c64384d6e56b
-- C4 -- <travis@c4ios.com>
2015-04-08 14:19:49 -0700

    Adds new animation keys to C4ShapeLayer, fixes minor bug in C4Vector for calculating angleTo.

--- C4UI/C4ShapeLayer.swift
+++ C4UI/C4ShapeLayer.swift
@@ -12,6 +12,21 @@ public class C4ShapeLayer: CAShapeLayer {
             animation.fromValue = self.lineWidth
             return animation;
         }
+        else if key == "strokeEnd" {
+            let animation = CABasicAnimation(keyPath: key)
+            animation.fromValue = self.strokeEnd
+            return animation;
+        }
+        else if key == "strokeStart" {
+            let animation = CABasicAnimation(keyPath: key)
+            animation.fromValue = self.strokeStart
+            return animation;
+        }
+        else if key == "path" {
+            let animation = CABasicAnimation(keyPath: key)
+            animation.fromValue = self.path
+            return animation;
+        }
         
         return super.actionForKey(key)
     }
